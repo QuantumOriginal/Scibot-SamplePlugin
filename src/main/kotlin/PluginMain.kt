6a352be5f480cfb674d7257be1d36154f54e8cfa
glowingstone124 <hanserofficial@outlook.com>
2024-11-08 12:23:34 +0800

    final

--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -5,6 +5,7 @@ import java.util.logging.Level
 class PluginMain : Interfaces.Plugin {
     var sender: Interfaces.SimpleSender? = null
     var logger: Interfaces.SimpleLogger? = null
+    var host: HostOperations? = null
     /*
     This is the sample plugin of SciBot.
     Define Main class and Plugin Name in resources/plugin.yml
@@ -12,12 +13,19 @@ class PluginMain : Interfaces.Plugin {
     implements Plugin interface to create a main class.
      */
     override suspend fun start() {
-        if(logger != null){
+        if(logger == null){
             throw IllegalArgumentException("logger isn't given")
         }
-        if(sender != null){
+        if(sender == null){
             throw IllegalArgumentException("sender isn't given")
         }
+        if(host == null){
+            throw IllegalArgumentException("host isn't given")
+        }
+        val result = host!!.getCurrentLogin()
+        val qq_search = host!!.getStrangerInfo(1294915648, true)
+        logger!!.log("Login with ${result.nickname}")
+        logger!!.log("Nickname of 1294915648: ${qq_search.nickname}")
         logger!!.log("hello,World", Level.INFO)
         sender!!.plainSend("hello,world", Sender.Type.GROUP,946085440)
         sender!!.send(mutableListOf(Events.AtMessage(1294915648L)), Sender.Type.GROUP, 946085440)
@@ -36,5 +44,8 @@ class PluginMain : Interfaces.Plugin {
     }
     fun getSender(sender: Interfaces.SimpleSender){
         this.sender = sender
+    }
+    fun getHost(host: HostOperations){
+        this.host = host;
     }
 }
